d2b4e5f16d056da4159da0e4d6b1321662584e1b
Mohammed Salmi <elmonstromail@gmail.com>
2022-10-25 12:20:43 -0400

    Refactor code by using a single event listener

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-//select dom elements
 const spacing = document.getElementById("spacing");
 const blur = document.getElementById("blur");
 const color = document.getElementById("base");
 const root = document.querySelector(":root");
-//get user values and set elements to them
-spacing.addEventListener("input", (e) => {
-  const range = e.target.value;
-  root.style.setProperty("--spacing", `${range}px`);
-});
 
-blur.addEventListener("input", (e) => {
-  const range = e.target.value;
-  root.style.setProperty("--blur", `${range}px`);
-});
-
-color.addEventListener("input", (e) => {
-  const colorPick = e.target.value;
-  root.style.setProperty("--base", colorPick);
+document.addEventListener("input", (e) => {
+  if (!e.target.matches("input")) return;
+  const userInput = e.target.value;
+  const variable = `--${e.target.name}`;
+  const variableValue = variable === "--base" ? userInput : `${userInput}px`;
+  root.style.setProperty(variable, variableValue);
 });
